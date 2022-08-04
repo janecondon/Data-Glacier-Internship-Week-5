21120e78c2096be47005dc0cc9f0ac799c8c6058
janecondon <90736883+janecondon@users.noreply.github.com>
2022-08-03 20:03:17 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = pickle.load(open('my_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index_1.html')
+    return render_template('index_1.HTML')
 
 @app.route('/predict',methods=['POST'])
 def predict():
